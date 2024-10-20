b199fc86dde4d9890b81057c1228837bc13ed6a3
AsthaKirar <asthap602@gmail.com>
2024-10-20 12:58:15 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Link, Routes,Route } from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
 
+const App = () => {
+  const getproducts = async ()=>{
+    
+    try{ 
+      const{ data } = await axios.get(
+          "https://fakestoreapi.com/products"
+
+      );
+      console.log(data);
+    }catch(error){
+      console.log(error);
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/About">About</Link>
+
+      
+    </nav>
+    <Routes>
+      <Route path="/" element ={<Home/>}></Route>
+      <Route path="/About" element ={<About/>}></Route>
+
+    </Routes>
+    <button onClick={getproducts}>Products</button>
     </>
   )
 }
